35496542a08aac2ef9fc1a4fe5fc31c6e0ef579b
Aaron Chen <aaron880221@gmail.com>
2020-07-22 20:19:47 +0800

    First Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+var app = express();
+const PORT = process.env.PORT || 8000;
+app.use("/", express.static(__dirname));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/test.html");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
