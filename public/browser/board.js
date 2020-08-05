b806830a2c219b94e5ac36f52662694cf70ce37c
Aaron Chen <aaron880221@gmail.com>
2020-08-05 16:35:37 +0800

    Add event listener

--- public/browser/board.js
+++ public/browser/board.js
@@ -11,23 +11,51 @@ function Board(height, width) {
 }
 
 Board.prototype.initialize = function () {
-    this.createGrid();
+    this.create_grid();
+    this.add_event_listener();
 };
 
-Board.prototype.createGrid = function () {
+Board.prototype.create_grid = function () {
     let tableContent = "";
     for (var row = 0; row < this.height; row++) {
         let rowContent = `<tr id="row${row}">`;
         for (var column = 0; column < this.width; column++) {
+            // Create table's html
             let nodeID = `${row}-${column}`;
-            let nodeClass = "unvisited";
+            let nodeClass = this.find_node_class(row, column);
             rowContent += `<td id="${nodeID}" class="${nodeClass}"></td>`;
         }
         tableContent += `${rowContent}</tr>`;
     }
     let board = document.getElementById("board");
     board.innerHTML = tableContent;
-    console.log(tableContent);
+};
+
+Board.prototype.find_node_class = function (row, column) {
+    if (row === Math.floor(this.height / 2) && column === Math.floor(this.width / 4)) {
+        return "start";
+    } else if (row === Math.floor(this.height / 2) && column === Math.floor((3 * this.width) / 4)) {
+        return "target";
+    } else {
+        return "unvisited";
+    }
+};
+
+Board.prototype.add_event_listener = function () {
+    for (var row = 0; row < this.height; row++) {
+        for (var column = 0; column < this.width; column++) {
+            let nodeID = `${row}-${column}`;
+            let currentNode = document.getElementById(nodeID);
+            currentNode.addEventListener("click", (event) => {
+                let status = currentNode.className;
+                if (status === "unvisited") {
+                    currentNode.className = "wall";
+                } else if (status === "wall") {
+                    currentNode.className = "unvisited";
+                }
+            });
+        }
+    }
 };
 
 Board.drawShortPath = function () {};
@@ -38,10 +66,9 @@ Board.clearPath = function () {};
 
 Board.clearBoard = function () {};
 
-let width = 20;
-let height = 25;
+let width = 50;
+let height = 13;
 // let width = Math.floor(document.getElementById("board").offsetWidth / 25);
 // let height = Math.floor(document.getElementById("board").offsetHeight / 20);
-console.log(width, height);
 let newBoard = new Board(height, width);
 newBoard.initialize();
