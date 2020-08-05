cec35b5f6255f434192d092e8fea2a7eb61e1b14
Aaron Chen <aaron880221@gmail.com>
2020-08-05 15:28:53 +0800

    create grid

--- public/browser/board.js
+++ public/browser/board.js
@@ -10,19 +10,24 @@ function Board(height, width) {
     this.boardTwoD = [];
 }
 
-Board.createBoard = function () {
-    var webWide = 1000;
-    var webHeight = 1000;
-    var board = [];
-    var table = document.getElementById("board");
-    for (var row = 0; row < webWide; row++) {
-        var row = table.insertRow(row);
-        var tempt = [];
-        for (var col = 0; col < webHeight; col++) {
-            var cell = row.insertCell(col);
-            cell.innerHTML = "HELLO";
+Board.prototype.initialize = function () {
+    this.createGrid();
+};
+
+Board.prototype.createGrid = function () {
+    let tableContent = "";
+    for (var row = 0; row < this.height; row++) {
+        let rowContent = `<tr id="row${row}">`;
+        for (var column = 0; column < this.width; column++) {
+            let nodeID = `${row}-${column}`;
+            let nodeClass = "unvisited";
+            rowContent += `<td id="${nodeID}" class="${nodeClass}"></td>`;
         }
+        tableContent += `${rowContent}</tr>`;
     }
+    let board = document.getElementById("board");
+    board.innerHTML = tableContent;
+    console.log(tableContent);
 };
 
 Board.drawShortPath = function () {};
@@ -33,4 +38,10 @@ Board.clearPath = function () {};
 
 Board.clearBoard = function () {};
 
-module.exports = Board;
+let width = 20;
+let height = 25;
+// let width = Math.floor(document.getElementById("board").offsetWidth / 25);
+// let height = Math.floor(document.getElementById("board").offsetHeight / 20);
+console.log(width, height);
+let newBoard = new Board(height, width);
+newBoard.initialize();
