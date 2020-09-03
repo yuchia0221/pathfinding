615f2b6b14395843b52de036c2b7cbfb4e5dc2d6
yuccching <68643802+yuccching@users.noreply.github.com>
2020-09-03 19:20:38 +0800

    improve Astar

--- public/browser/Algorithms/AStar.js
+++ public/browser/Algorithms/AStar.js
@@ -1,7 +1,7 @@
 function AStar(start, target, boardTwoD, visitedList) {
     var priorityQueue = Array();
     var previousNode = null;
-    set_dist(start, boardTwoD, priorityQueue);
+    set_dist(target, start, boardTwoD, priorityQueue);
 
     while (priorityQueue.length) {
         var currentNode = priorityQueue.shift();
@@ -68,22 +68,26 @@ function checkChild(target, targetChild, currentNode, priorityQueue) {
             currentNode.totalDist +
             dist(target.row, target.column, targetChild.row, targetChild.column) +
             targetChild.weight;
+
         targetChild.dist = temp;
-        targetChild.father = currentNode;
+        if (targetChild.father === null) {
+            targetChild.father = currentNode;
+        }
+
         // targetChild.father = currentNode;
 
         insert_to_queue(targetChild, priorityQueue);
-        
+
     }
 }
 
-function set_dist(start, boardTwoD, priorityQueue) {
+function set_dist(target, start, boardTwoD, priorityQueue) {
     var boardHeight = boardTwoD.length;
     var boardWidth = boardTwoD[0].length;
     for (var i = 0; i < boardHeight; i++) {
         for (var j = 0; j < boardWidth; j++) {
             if (boardTwoD[i][j].location === start.location) {
-                boardTwoD[i][j].dist = 0;
+                boardTwoD[i][j].dist = dist(start.row, start.column, target.row, target.column);
                 insert_to_queue(boardTwoD[i][j], priorityQueue);
             }
             boardTwoD[i][j].dist = Infinity;
